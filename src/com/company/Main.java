4c5239efa71dcf2debc671b44ef6fc1f8405aceb
gbarth1 <grace.m.barth@gmail.com>
2016-12-16 14:28:33 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	    //create board here
+
+        System.out.println("Who is playing first? X or O?");
+        Scanner kb= new Scanner(System.in);
+        String ans=kb.next();
+
+        //System.out.println("Pick your first spot/move" + " " + ans + ". Ex: (1,1), (2,3), (3,2)");
+        //possibly have player enter in column and row seperatly 
+    }
+}

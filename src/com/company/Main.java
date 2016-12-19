6e57b0a022fcdb82f27cf63526afbf186e8addb3
gbarth1 <grace.m.barth@gmail.com>
2016-12-19 14:29:15 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,16 @@ public class Main
 
     public static void main(String[] args)
     {
-	    //create board here
-
+        Object[] br= new Object[3];
+        br[1]=new Integer[]{}
+        //System.out.println();
         System.out.println("Who is playing first? X or O?");
         Scanner kb= new Scanner(System.in);
         String ans=kb.next();
 
         //System.out.println("Pick your first spot/move" + " " + ans + ". Ex: (1,1), (2,3), (3,2)");
-        //possibly have player enter in column and row seperatly 
+        //possibly have player enter in column and row seperatly
+
+
     }
 }
---
+++ src/com/company/board.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by gb647 on 12/19/16.
+ */
+public class board {
+
+    public void ttt() {
+        int[][] brd = new int[3][3];
+
+    }
+}
